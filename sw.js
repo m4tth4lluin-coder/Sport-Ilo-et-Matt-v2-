01d5c0f3fed832dbace91fc82d561b6a0ddffd94
m4tth4lluin-coder <m4tt.h4lluin@gmail.com>
2026-09-21 01:52:29 +0200

    Refresh cache for difficulty convention

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="lafay-trx-lab-v2";
+const CACHE="lafay-trx-lab-v3";
 const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
